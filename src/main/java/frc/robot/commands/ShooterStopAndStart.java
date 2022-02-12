ddf67316bde063022d80ad850a5ab6622731c1a2
Ethan Himmell <ethanhimmell58@gmail.com>
2022-02-11 16:24:10 -0800

    start of changes

---
+++ src/main/java/frc/robot/commands/ShooterStopAndStart.java
@@ -0,0 +1,27 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.ShooterSubSystem;
+
+public class ShooterStopAndStart extends CommandBase {
+    private ShooterSubSystem m_motor1;
+    private boolean checkState = true;
+    public ShooterStopAndStart(ShooterSubSystem motor1) {
+       m_motor1 = motor1;
+       addRequirements(m_motor1);
+    }
+
+    @Override 
+    public void execute() {
+        m_motor1.run();
+        checkState = false;
+    }
+
+    @Override
+    public boolean isFinished() {
+        return !checkState;
+    }
+
+    @Override
+    public void end(boolean interrupted) {}
+}
