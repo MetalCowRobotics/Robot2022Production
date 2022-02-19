9523435a525c2dfafe4bcb0e0314e66c8f00f930
Quantum357 <59151112+Quantum357@users.noreply.github.com>
2022-02-19 11:43:29 -0600

    Added PID to single shooter motor

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -17,22 +17,22 @@ import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.ShooterSubSystem;
 
 public class RobotContainer {
-  private final DrivetrainSubsystem m_drivetrainSubsystem = new DrivetrainSubsystem();
+  // private final DrivetrainSubsystem m_drivetrainSubsystem = new DrivetrainSubsystem();
   private final XboxController driverControls = new XboxController(0);
   private final XboxController operatorControls = new XboxController(1);
-  private final IntakeSubsystem m_intakeSubsystem = new IntakeSubsystem();
-  private final ClimberSubsystem m_climberSubsystem = new ClimberSubsystem();
+  // private final IntakeSubsystem m_intakeSubsystem = new IntakeSubsystem();
+  // private final ClimberSubsystem m_climberSubsystem = new ClimberSubsystem();
   private final ShooterSubSystem m_ShooterSubSystem = new ShooterSubSystem();
 
   private final double CONTROLLER_DEADBAND = 0.1;
   
   public RobotContainer() {
 
-    m_drivetrainSubsystem.setDefaultCommand(new DefaultDriveCommand(
-            m_drivetrainSubsystem,
-            () -> -modifyAxis(deadband(driverControls.getLeftX(), CONTROLLER_DEADBAND) * DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND),
-            () -> modifyAxis(deadband(driverControls.getLeftY(), CONTROLLER_DEADBAND) * DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND),
-            () -> -modifyAxis(deadband(driverControls.getRightX(), CONTROLLER_DEADBAND) * DrivetrainSubsystem.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND)));
+    // m_drivetrainSubsystem.setDefaultCommand(new DefaultDriveCommand(
+    //         m_drivetrainSubsystem,
+    //         () -> -modifyAxis(deadband(driverControls.getLeftX(), CONTROLLER_DEADBAND) * DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND),
+    //         () -> modifyAxis(deadband(driverControls.getLeftY(), CONTROLLER_DEADBAND) * DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND),
+    //         () -> -modifyAxis(deadband(driverControls.getRightX(), CONTROLLER_DEADBAND) * DrivetrainSubsystem.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND)));
       // Configure the button bindings
       configureButtonBindings();
   }
@@ -40,35 +40,35 @@ public class RobotContainer {
   private void configureButtonBindings() {
 
 	  //Reset Gyro
-    new Button(driverControls::getBackButton).whenPressed(m_drivetrainSubsystem::zeroGyroscope);
+    // new Button(driverControls::getBackButton).whenPressed(m_drivetrainSubsystem::zeroGyroscope);
 
-	  //Crawl
-    new Button(driverControls::getLeftBumper).whenPressed(m_drivetrainSubsystem::crawl);
-    new Button(driverControls::getLeftBumper).whenReleased(m_drivetrainSubsystem::resetSpeed);
+	  // //Crawl
+    // new Button(driverControls::getLeftBumper).whenPressed(m_drivetrainSubsystem::crawl);
+    // new Button(driverControls::getLeftBumper).whenReleased(m_drivetrainSubsystem::resetSpeed);
 
 	  //Sprint
-	  new Button(driverControls::getRightBumper).whenPressed(m_drivetrainSubsystem::sprint);
-    new Button(driverControls::getRightBumper).whenReleased(m_drivetrainSubsystem::resetSpeed);
+	  // new Button(driverControls::getRightBumper).whenPressed(m_drivetrainSubsystem::sprint);
+    // new Button(driverControls::getRightBumper).whenReleased(m_drivetrainSubsystem::resetSpeed);
 
-    //Switch Field Mode
-	  new Button(operatorControls::getBackButton).whenPressed(m_climberSubsystem::switchFieldMode);
+    // //Switch Field Mode
+	  // new Button(operatorControls::getBackButton).whenPressed(m_climberSubsystem::switchFieldMode);
 
-		new Button(operatorControls::getRightBumper).whenPressed(m_climberSubsystem::extendClimberMotor);
-		new Button(operatorControls::getRightBumper).whenReleased(m_climberSubsystem::stopClimberMotor);
+		// new Button(operatorControls::getRightBumper).whenPressed(m_climberSubsystem::extendClimberMotor);
+		// new Button(operatorControls::getRightBumper).whenReleased(m_climberSubsystem::stopClimberMotor);
 
-		new Button(operatorControls::getLeftBumper).whenPressed(m_climberSubsystem::retractClimberMotor);
-		new Button(operatorControls::getLeftBumper).whenReleased(m_climberSubsystem::stopClimberMotor);
+		// new Button(operatorControls::getLeftBumper).whenPressed(m_climberSubsystem::retractClimberMotor);
+		// new Button(operatorControls::getLeftBumper).whenReleased(m_climberSubsystem::stopClimberMotor);
 
-		new Button(operatorControls::getAButton).whenPressed(m_climberSubsystem::deployClimber);
-		new Button(operatorControls::getBButton).whenPressed(m_climberSubsystem::retractClimber);
+		// new Button(operatorControls::getAButton).whenPressed(m_climberSubsystem::deployClimber);
+		// new Button(operatorControls::getBButton).whenPressed(m_climberSubsystem::retractClimber);
 
     //Shoot
     new Button(operatorControls::getRightBumper).whenPressed(m_ShooterSubSystem::run);
     new Button(operatorControls::getRightBumper).whenReleased(m_ShooterSubSystem::stop);
 
     // SmartDashboard.putData("Prepare to Gather", new PrepareIntakeToGather(m_intakeSubsystem));
-    SmartDashboard.putData("Retract Intake", new InstantCommand(m_intakeSubsystem::retractIntake, m_intakeSubsystem));
-    SmartDashboard.putData("Neutral Intake", new InstantCommand(m_intakeSubsystem::neutralIntake, m_intakeSubsystem));
+    // SmartDashboard.putData("Retract Intake", new InstantCommand(m_intakeSubsystem::retractIntake, m_intakeSubsystem));
+    // SmartDashboard.putData("Neutral Intake", new InstantCommand(m_intakeSubsystem::neutralIntake, m_intakeSubsystem));
   }
 
   /**
@@ -78,9 +78,10 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // Command autoCommand = new DriveStraight(0, 0.3, m_drivetrainSubsystem, 12);
-    Command autoCommand = new DriveToCoordinate(m_drivetrainSubsystem, 0, 1);
+    // Command autoCommand = new DriveToCoordinate(m_drivetrainSubsystem, 0, 1);
     // An ExampleCommand will run in autonomous
-    return autoCommand;
+    // return autoCommand;
+    return null;
   }
 
   private static double deadband(double value, double deadband) {
--- src/main/java/frc/robot/subsystems/ShooterSubSystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubSystem.java
@@ -1,43 +1,92 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkMaxPIDController;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
-public class ShooterSubSystem extends SubsystemBase{
-    private CANSparkMax shootMotor = new CANSparkMax(Constants.SHOOTER_MOTOR, MotorType.kBrushless);
-    private final double wantedSpeed = 0.2;
 
-    private double speed = 0;
+public class ShooterSubSystem extends SubsystemBase {
+    private CANSparkMax shootMotor = new CANSparkMax(Constants.SHOOTER_MOTOR, MotorType.kBrushless);
+    private RelativeEncoder encoder = shootMotor.getEncoder();
+    private double targetSpeed = 0;
+    private SparkMaxPIDController pid;
+    private double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput;
 
     public ShooterSubSystem() {
-        
+        pid = shootMotor.getPIDController();
+
+        SmartDashboard.putNumber("Shooter Target", 300);
+
+        // PID coefficients
+        kP = 0.00006;
+        kI = 0.000001;
+        kD = 0.0001;
+        kIz = 10000;
+        kFF = 0.000015;
+        kMaxOutput = 1;
+        kMinOutput = -1;
+
+        // set PID coefficients
+        pid.setP(kP);
+        pid.setI(kI);
+        pid.setD(kD);
+        pid.setIZone(kIz);
+        pid.setFF(kFF);
+        pid.setOutputRange(kMinOutput, kMaxOutput);
+
+        // display PID coefficients on SmartDashboard
+        SmartDashboard.putNumber("P Gain", kP);
+        SmartDashboard.putNumber("I Gain", kI);
+        SmartDashboard.putNumber("D Gain", kD);
+        SmartDashboard.putNumber("I Zone", kIz);
+        SmartDashboard.putNumber("Feed Forward", kFF);
+        SmartDashboard.putNumber("Max Output", kMaxOutput);
+        SmartDashboard.putNumber("Min Output", kMinOutput);
     }
 
     public void run() {
-        speed = 0.2;
+        targetSpeed = SmartDashboard.getNumber("Shooter Target", 0);
     }
 
     public void stop() {
-        speed = 0;
+        targetSpeed = 0;
     }
 
     @Override
-    
+
     public void periodic() {
-        shootMotor.set(speed);
-        SmartDashboard.putNumber("Speed", shootMotor.get());
+        // read PID coefficients from SmartDashboard
+        double p = SmartDashboard.getNumber("P Gain", 0);
+        double i = SmartDashboard.getNumber("I Gain", 0);
+        double d = SmartDashboard.getNumber("D Gain", 0);
+        double iz = SmartDashboard.getNumber("I Zone", 0);
+        double ff = SmartDashboard.getNumber("Feed Forward", 0);
+        double max = SmartDashboard.getNumber("Max Output", 0);
+        double min = SmartDashboard.getNumber("Min Output", 0);
+
+        // if PID coefficients on SmartDashboard have changed, write new values to controller
+        if((p != kP)) { pid.setP(p); kP = p; }
+        if((i != kI)) { pid.setI(i); kI = i; }
+        if((d != kD)) { pid.setD(d); kD = d; }
+        if((iz != kIz)) { pid.setIZone(iz); kIz = iz; }
+        if((ff != kFF)) { pid.setFF(ff); kFF = ff; }
+        if((max != kMaxOutput) || (min != kMinOutput)) { 
+            pid.setOutputRange(min, max); 
+            kMinOutput = min; kMaxOutput = max; 
+        }
+
+        pid.setReference(targetSpeed, CANSparkMax.ControlType.kVelocity);
+        SmartDashboard.putNumber("Current Speed", encoder.getVelocity());
         SmartDashboard.putBoolean("isReady", isReady());
-        SmartDashboard.putNumber("WantedSpeed", wantedSpeed);
     }
 
-    public boolean isReady(){
-        if (shootMotor.get() >= wantedSpeed){
-            return true;        
+    public boolean isReady() {
+        if (encoder.getVelocity() >= targetSpeed) {
+            return true;
         } else {
             return false;
         }
