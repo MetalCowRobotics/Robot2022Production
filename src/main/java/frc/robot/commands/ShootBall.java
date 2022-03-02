4ebd9691614254fd7892a42eef312c86f96f1a22
dean <deaner8765@aol.com>
2022-03-01 19:08:53 -0600

    added ShootBall

---
+++ src/main/java/frc/robot/commands/ShootBall.java
@@ -0,0 +1,25 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandGroupBase;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.MagazineSubsystem;
+import frc.robot.subsystems.ShooterSubSystem;
+import frc.robot.subsystems.DrivetrainSubsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class ShootBall extends SequentialCommandGroup{
+
+    public ShootBall(ShooterSubSystem shooter, DrivetrainSubsystem drive, double Delay){
+        addCommands(
+            new DoDelay(Delay),
+
+            new DriveToCoordinate(drive, SmartDashboard.getNumber("x Amount", 0.5), SmartDashboard.getNumber("y Amount", 0.5))
+        );
+    
+    }
+
+    
+
+    
+}
