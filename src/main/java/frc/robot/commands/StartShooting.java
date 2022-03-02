0a9a9c8a76d272ec5ffcdf5811609bd6dd557f9f
dean <deaner8765@aol.com>
2022-03-01 20:35:13 -0600

    merged both Magazine and command branches into one branch

--- src/main/java/frc/robot/commands/StartShooting.java
+++ src/main/java/frc/robot/commands/StartShooting.java
@@ -3,12 +3,12 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.IntakeSubsystem;
-import frc.robot.subsystems.Magazine;
-import frc.robot.subsystems.ShooterSubSystem;
+import frc.robot.subsystems.MagazineSubsystem;
+import frc.robot.subsystems.ShooterSubsystem;
 
 public class StartShooting extends SequentialCommandGroup {
 
-    public StartShooting(Magazine mag) {
+    public StartShooting(MagazineSubsystem mag) {
         // add command to put drivetrain on max brake
         addCommands(new StartMagazine(mag));
     }
--- src/main/java/frc/robot/commands/StopShooterWheel.java
+++ src/main/java/frc/robot/commands/StopShooterWheel.java
@@ -1,12 +1,12 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.subsystems.ShooterSubSystem;
+import frc.robot.subsystems.ShooterSubsystem;
 
-public class ShooterStop extends CommandBase {
-    private ShooterSubSystem m_motor1;
+public class StopShooterWheel extends CommandBase {
+    private ShooterSubsystem m_motor1;
     private boolean checkState = true;
-    public ShooterStop(ShooterSubSystem motor1) {
+    public StopShooterWheel(ShooterSubsystem motor1) {
        m_motor1 = motor1;
        addRequirements(m_motor1);
     }
