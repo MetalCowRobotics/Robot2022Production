99961bd3e20fc8d828ee098feee8d40ae07584fc
Alex-ops-prog <59714357+Alex-ops-prog@users.noreply.github.com>
2022-03-12 13:44:54 -0600

    TestingForTestFrame
    The testing code for the test frame, commented out the subsystem instances.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -25,26 +25,26 @@ import frc.robot.subsystems.MagazineSubsystem;
 public class RobotContainer {
   private final XboxController driverControls = new XboxController(0);
   private final XboxController operatorControls = new XboxController(1);
-  private final DrivetrainSubsystem m_drivetrainSubsystem = new DrivetrainSubsystem();
-  private final IntakeSubsystem m_intakeSubsystem = new IntakeSubsystem();
-  private final ClimberSubsystem m_climberSubsystem = new ClimberSubsystem();
-  private final ShooterSubsystem m_ShooterSubsystem = new ShooterSubsystem();
-  private final MagazineSubsystem m_magazineSubsystem = new MagazineSubsystem();
+  // private final DrivetrainSubsystem m_drivetrainSubsystem = new DrivetrainSubsystem();
+  // private final IntakeSubsystem m_intakeSubsystem = new IntakeSubsystem();
+  // private final ClimberSubsystem m_climberSubsystem = new ClimberSubsystem();
+  // private final ShooterSubsystem m_ShooterSubsystem = new ShooterSubsystem();
+  // private final MagazineSubsystem m_magazineSubsystem = new MagazineSubsystem();
   private final ToggleCameraFeed m_toggleCameraFeed = new ToggleCameraFeed();
   private double delay = 0;
-  private final ShootBall m_shootball = new ShootBall(m_ShooterSubsystem, m_drivetrainSubsystem, delay);
-  SendableChooser m_chooser = new SendableChooser();
+  // private final ShootBall m_shootball = new ShootBall(m_ShooterSubsystem, m_drivetrainSubsystem, delay);
+  // SendableChooser m_chooser = new SendableChooser();
 
   private final double CONTROLLER_DEADBAND = 0.1;
 
   public RobotContainer() {
 
-    m_drivetrainSubsystem.setDefaultCommand(new DefaultDriveCommand(
-            m_drivetrainSubsystem,
-            () -> -modifyAxis(deadband(driverControls.getLeftX(), CONTROLLER_DEADBAND) * DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND),
-            () -> modifyAxis(deadband(driverControls.getLeftY(), CONTROLLER_DEADBAND) * DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND),
-            () -> -modifyAxis(deadband(driverControls.getRightX(), CONTROLLER_DEADBAND) * DrivetrainSubsystem.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND)));
-      // Configure the button bindings
+    // m_drivetrainSubsystem.setDefaultCommand(new DefaultDriveCommand(
+    //         m_drivetrainSubsystem,
+    //         () -> -modifyAxis(deadband(driverControls.getLeftX(), CONTROLLER_DEADBAND) * DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND),
+    //         () -> modifyAxis(deadband(driverControls.getLeftY(), CONTROLLER_DEADBAND) * DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND),
+    //         () -> -modifyAxis(deadband(driverControls.getRightX(), CONTROLLER_DEADBAND) * DrivetrainSubsystem.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND)));
+    //   // Configure the button bindings
       configureButtonBindings();
 
       // m_chooser.setDefaultOption("Drive to -1,0", new DriveToCoordinate(m_drivetrainSubsystem, -1, 0));
@@ -54,57 +54,58 @@ public class RobotContainer {
       // m_chooser.addOption("Delay Drive Forward", m_shootball);
       SmartDashboard.putNumber("Delay", delay);
 
-      SmartDashboard.putData("Autonomous Command", m_chooser);
+      // SmartDashboard.putData("Autonomous Command", m_chooser);
   }
 
   public Command getAutoCommand(){
-    return (Command) m_chooser.getSelected();
+    // return (Command) m_chooser.getSelected();
+    return null;
   }
 
   private void configureButtonBindings() {
 
-    //Driver
-		//Reset Gyro
-		Constants.CONT_RESET_GYRO.whenPressed(m_drivetrainSubsystem::zeroGyroscope);
+  //   //Driver
+	// 	//Reset Gyro
+	// 	Constants.CONT_RESET_GYRO.whenPressed(m_drivetrainSubsystem::zeroGyroscope);
 
-		//Crawl
-		Constants.CONT_CRAWL.whenPressed(m_drivetrainSubsystem::crawl);
-		Constants.CONT_CRAWL.whenReleased(m_drivetrainSubsystem::resetSpeed);
+	// 	//Crawl
+	// 	Constants.CONT_CRAWL.whenPressed(m_drivetrainSubsystem::crawl);
+	// 	Constants.CONT_CRAWL.whenReleased(m_drivetrainSubsystem::resetSpeed);
 
-		//Sprint
-		Constants.CONT_SPRINT.whenPressed(m_drivetrainSubsystem::sprint);
-		Constants.CONT_SPRINT.whenReleased(m_drivetrainSubsystem::resetSpeed);
+	// 	//Sprint
+	// 	Constants.CONT_SPRINT.whenPressed(m_drivetrainSubsystem::sprint);
+	// 	Constants.CONT_SPRINT.whenReleased(m_drivetrainSubsystem::resetSpeed);
 
-		//Intake
-		Constants.CONT_INTAKE_DEPLOY.whenReleased(m_intakeSubsystem::deployIntake);
-		Constants.CONT_INTAKE_DEPLOY.whenReleased(m_intakeSubsystem::run);
-		Constants.CONT_INTAKE_RETRACT.whenReleased(m_intakeSubsystem::retractIntake);
-		Constants.CONT_INTAKE_RETRACT.whenReleased(m_intakeSubsystem::stop);
+	// 	//Intake
+	// 	Constants.CONT_INTAKE_DEPLOY.whenReleased(m_intakeSubsystem::deployIntake);
+	// 	Constants.CONT_INTAKE_DEPLOY.whenReleased(m_intakeSubsystem::run);
+	// 	Constants.CONT_INTAKE_RETRACT.whenReleased(m_intakeSubsystem::retractIntake);
+	// 	Constants.CONT_INTAKE_RETRACT.whenReleased(m_intakeSubsystem::stop);
 
 	//Operator
     //Switch Camera Feed
     Constants.CONT_SWITCH_CAMERA_1.whenPressed(m_toggleCameraFeed::switchToCamera1);
     Constants.CONT_SWITCH_CAMERA_2.whenPressed(m_toggleCameraFeed::switchToCamera2);
 
-		//Switch Field Mode
-		Constants.CONT_SWITCH_FIELD_MODE.whenPressed(m_climberSubsystem::switchFieldMode);
+	// 	//Switch Field Mode
+	// 	Constants.CONT_SWITCH_FIELD_MODE.whenPressed(m_climberSubsystem::switchFieldMode);
 
-		//Climb
-		Constants.CONT_CLIMBER_UP.whenPressed(m_climberSubsystem::extendClimberMotor);
-		Constants.CONT_CLIMBER_UP.whenReleased(m_climberSubsystem::stopClimberMotor);
+	// 	//Climb
+	// 	Constants.CONT_CLIMBER_UP.whenPressed(m_climberSubsystem::extendClimberMotor);
+	// 	Constants.CONT_CLIMBER_UP.whenReleased(m_climberSubsystem::stopClimberMotor);
 
-		Constants.CONT_CLIMBER_DOWN.whenPressed(m_climberSubsystem::retractClimberMotor);
-		Constants.CONT_CLIMBER_DOWN.whenReleased(m_climberSubsystem::stopClimberMotor);
+	// 	Constants.CONT_CLIMBER_DOWN.whenPressed(m_climberSubsystem::retractClimberMotor);
+	// 	Constants.CONT_CLIMBER_DOWN.whenReleased(m_climberSubsystem::stopClimberMotor);
 
-		Constants.CONT_CLIMBER_OUT.whenPressed(m_climberSubsystem::deployClimber);
-		Constants.CONT_CLIMBER_IN.whenPressed(m_climberSubsystem::retractClimber);
+	// 	Constants.CONT_CLIMBER_OUT.whenPressed(m_climberSubsystem::deployClimber);
+	// 	Constants.CONT_CLIMBER_IN.whenPressed(m_climberSubsystem::retractClimber);
 
-		//Shoot
-		Constants.CONT_SHOOTER_RUN.whenPressed(m_ShooterSubsystem::run);
-		Constants.CONT_SHOOTER_RUN.whenPressed(m_magazineSubsystem::loadContinuous);
+	// 	//Shoot
+	// 	Constants.CONT_SHOOTER_RUN.whenPressed(m_ShooterSubsystem::run);
+	// 	Constants.CONT_SHOOTER_RUN.whenPressed(m_magazineSubsystem::loadContinuous);
 
-		Constants.CONT_SHOOTER_RUN.whenReleased(m_ShooterSubsystem::stop);
-		Constants.CONT_SHOOTER_RUN.whenReleased(m_magazineSubsystem::stop);
+	// 	Constants.CONT_SHOOTER_RUN.whenReleased(m_ShooterSubsystem::stop);
+	// 	Constants.CONT_SHOOTER_RUN.whenReleased(m_magazineSubsystem::stop);
 
 
     //Shooter Command Group
