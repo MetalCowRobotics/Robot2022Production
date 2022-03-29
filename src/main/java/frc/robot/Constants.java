d14965237d1979af5a3257c779fe06bd5cfcee5b
Quantum357 <59151112+Quantum357@users.noreply.github.com>
2022-03-29 18:40:37 -0500

    swapped drive and steering motors

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -46,26 +46,26 @@ public final class Constants {
         public static final int DRIVETRAIN_PIGEON_ID = 14;
 
         //Front Left Module
-        public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 8;
-        public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 7;
+        public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 7;
+        public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 8;
         public static final int FRONT_LEFT_MODULE_STEER_ENCODER = 9;
         public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(59.6 + 180);
 
         //Front Right Module
-        public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 5;
-        public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 4;
+        public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 4;
+        public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 5;
         public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 6;
         public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(1.5);
 
         //Back Left Module
-        public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 11;
-        public static final int BACK_LEFT_MODULE_STEER_MOTOR = 10;
+        public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 10;
+        public static final int BACK_LEFT_MODULE_STEER_MOTOR = 11;
         public static final int BACK_LEFT_MODULE_STEER_ENCODER = 12;
         public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(345.4 + 180);
 
         //Back Right Module
-        public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 2;
-        public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 1;
+        public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 1;
+        public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 2;
         public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 3;
         public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(94.5);
 
@@ -90,8 +90,8 @@ public final class Constants {
 
     //Climber
 
-    public static final double CLIMB_SPEED_IN = 0.4;
-    public static final double CLIMB_SPEED_OUT = 0.8;
+    public static final double CLIMB_SPEED_IN = 0.6;
+    public static final double CLIMB_SPEED_OUT = 1;
 
     //Shooter
     public static final double SHOOTER_BASE_SPEED = 3200;
