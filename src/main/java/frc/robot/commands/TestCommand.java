8741fbc08e8de7c7ef574b45c5084d9b5380978e
Jake <jaketc05@gmail.com>
2023-01-14 10:58:14 -0600

    Update TestCommand.java

---
+++ src/main/java/frc/robot/commands/TestCommand.java
@@ -0,0 +1,31 @@
+
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.TestSubsystem;
+
+
+public class TestCommand extends CommandBase {
+  // The subsystem the command runs on
+  private final TestSubsystem m_TestSubsystem;
+
+  public TestCommand(TestSubsystem subsystem) {
+    m_TestSubsystem = subsystem;
+    addRequirements(m_TestSubsystem);
+  }
+
+  @Override
+  public void initialize() {
+  }
+
+  @Override
+  public void execute(){
+    m_TestSubsystem.
+  }
+
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
